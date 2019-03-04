92bd15df5ff5882146671781b55357d977193b3a
Alex Bardashevsky <mijiekc@icloud.com>
2019-03-04 10:07:04 +0200

    Initial Commit

---
+++ New - 37. MKMapView Part 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New - 37. MKMapView Part 1
+//
+//  Created by Oleksandr Bardashevskyi on 3/4/19.
+//  Copyright © 2019 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
